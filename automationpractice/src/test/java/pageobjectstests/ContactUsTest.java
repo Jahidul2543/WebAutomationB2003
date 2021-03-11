209a47596aa4f4f97afdd636c6eaaeaddfc75151
jahidul2543 <jahidul2543@gmail.com>
2021-03-10 21:15:58 -0500

    updated BrowserDriver, added datadriven approach

--- automationpractice/src/test/java/pageobjectstests/ContactUsTest.java
+++ automationpractice/src/test/java/pageobjectstests/ContactUsTest.java
@@ -1,9 +1,11 @@
 package pageobjectstests;
 
 import browserdriver.BrowserDriver;
+import excelreader.MyDataReader;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pageobjects.ContactUsPage;
 import pageobjects.HomePage;
@@ -19,11 +21,20 @@ public class ContactUsTest extends BrowserDriver {
         contactUsPage = PageFactory.initElements(driver, ContactUsPage.class);
     }
 
-    @Test
-    public void sendAMessageTest()  {
+    @DataProvider
+    public Object[][] dataProvider() throws Exception {
+        MyDataReader myDataReader = new MyDataReader();
+        //Where is the excel file
+        myDataReader.setExcelFile("/Users/jahidul/IdeaProjects/WebAutomationB2003/automationpractice/testData/testdata1.xlsx");
+        Object[][] data = myDataReader.getExcelSheetData("Sheet1");
+        return data;
+    }
+
+    @Test(dataProvider = "dataProvider")
+    public void sendAMessageTest(String e, String m)  {
         homePage.clickContactUs();
-        contactUsPage.writeEmailValue("junk@kunk.com");
-        contactUsPage.sendText("Provide me some junk information!!!");
+        contactUsPage.writeEmailValue(e);
+        contactUsPage.sendText(m);
         contactUsPage.clickSubmitButton();
         String actualErrorMessage = contactUsPage.getErrorMessage();
         Assert.assertEquals(actualErrorMessage, "There is 1 error");
